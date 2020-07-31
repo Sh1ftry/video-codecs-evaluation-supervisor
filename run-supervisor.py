6e81511af133c74f82cd56f89a4430aca3cfc75a
Michal Padula <michal.padula@outlook.com>
2020-07-31 19:47:48 +0200

    Modify run supervisor

--- run-supervisor.py
+++ run-supervisor.py
@@ -2,34 +2,63 @@ from pathlib import Path
 import sys
 import subprocess
 
+logs_path = Path("./process.log")
+results_root_path = Path("./results")
 
-def extract_data(path):
-    parts = path.parts
-    modality = parts[len(parts) - 2]
-    collection = parts[len(parts) - 3]
-    number = parts[len(parts) - 1].split(".")[0]
-    return " ".join([str(path), modality, collection, number])
+data_root_paths = [Path("./data"), Path("./data-packed")]
+
+for data_root_path in data_root_paths:
+    paths = [str(path.absolute()) for path in list(data_root_path.rglob("*.cimg"))]
+    paths_path = data_root_path.with_suffix(".paths")
+    with open(paths_path, mode="wt", encoding="utf-8") as output:
+        output.write("\n".join(paths))
 
 
-data_root_path = sys.argv[1]
-output_path = sys.argv[2]
-exe_path = sys.argv[3]
-config_path = sys.argv[4]
-result_path = sys.argv[5]
-logs_file = sys.argv[6]
+class Encoder:
+    def __init__(self, name, config_path):
+        self.exe_path = Path(f"./{name}.sh").absolute()
+        self.results_path = results_root_path / name
+        self.config_path = config_path
 
-paths = [path.absolute() for path in list(Path(data_root_path).rglob("*.cimg"))]
-paths_with_data = [extract_data(path) for path in paths]
 
-with open(output_path, mode="wt", encoding="utf-8") as output:
-    output.write("\n".join(paths_with_data))
+encoders = [
+    Encoder("jp3d", None),
+    Encoder("HEVCEncoder", Path("hevc.cfg")),
+    Encoder("VVCEncoder", Path("vvc.cfg")),
+]
 
-lines = 0
-with open(result_path) as file:
-    lines = len(file.readlines()) - 1
-    if lines == -1:
+
+def count_lines(path):
+    lines = 0
+    try:
+        with open(path) as file:
+            lines = len(file.readlines()) - 1
+            if lines == -1:
+                lines = 0
+    except FileNotFoundError:
         lines = 0
+    return lines
 
-with open(logs_file, "a+") as logs:
-    exit_code = subprocess.call([exe_path, output_path, config_path, result_path, str(lines)], stdout=logs, stderr=logs)
-    print(f"Subprocess exited with {exit_code}")
+
+with open(logs_path, "a+") as logs:
+    for encoder in encoders:
+        for data_root_path in data_root_paths:
+            process_args = [
+                encoder.exe_path.absolute(),
+                data_root_path.with_suffix(".paths").absolute(),
+            ]
+            if encoder.config_path is not None:
+                process_args.append(encoder.config_path.absolute())
+            encoder.results_path.mkdir(parents=True, exist_ok=True)
+            results_path = (encoder.results_path / data_root_path.name).with_suffix(
+                ".csv"
+            )
+            processed_lines = count_lines(results_path)
+            process_args.append(results_path.absolute())
+            process_args.append(processed_lines)
+            process_args_stringified = [str(arg) for arg in process_args]
+            print(f"Launching subprocess {process_args_stringified}")
+            exit_code = subprocess.call(
+                process_args_stringified, stdout=logs, stderr=logs
+            )
+            print(f"{encoder.exe_path} subprocess exited with {exit_code}")
