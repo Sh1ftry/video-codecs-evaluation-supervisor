50c778631ca77b4f1ef22e64abd349b5a9a8a4f1
Michal Padula <michal.padula@outlook.com>
2020-08-03 17:19:53 +0200

    Change logs location

--- run-supervisor.py
+++ run-supervisor.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import sys
 import subprocess
 
-logs_path = Path("./process.log")
 results_root_path = Path("./results")
 
 data_root_paths = [Path("./data"), Path("./data-packed")]
@@ -16,7 +15,7 @@ for data_root_path in data_root_paths:
 
 class Encoder:
     def __init__(self, name, config_path):
-        self.exe_path = Path(f"./{name}.exe").absolute()
+        self.exe_path = Path(f"./{name}.sh").absolute()
         self.results_path = results_root_path / name
         self.config_path = config_path
 
@@ -40,21 +39,24 @@ def count_lines(path):
     return lines
 
 
-with open(logs_path, "a+") as logs:
-    for encoder in encoders:
-        for data_root_path in data_root_paths:
-            process_args = [
-                encoder.exe_path.absolute(),
-                data_root_path.with_suffix(".paths").absolute(),
-            ]
-            if encoder.config_path is not None:
-                process_args.append(encoder.config_path.absolute())
-            encoder.results_path.mkdir(parents=True, exist_ok=True)
-            results_path = (encoder.results_path / data_root_path.name).with_suffix(".csv")
-            processed_lines = count_lines(results_path)
-            process_args.append(results_path.absolute())
-            process_args.append(processed_lines)
-            process_args_stringified = [str(arg) for arg in process_args]
-            print(f"Launching subprocess {process_args_stringified}")
-            exit_code = subprocess.call(process_args_stringified, stdout=logs, stderr=logs)
+for encoder in encoders:
+    for data_root_path in data_root_paths:
+        process_args = [
+            encoder.exe_path.absolute(),
+            data_root_path.with_suffix(".paths").absolute(),
+        ]
+        if encoder.config_path is not None:
+            process_args.append(encoder.config_path.absolute())
+        encoder.results_path.mkdir(parents=True, exist_ok=True)
+        results_path = (encoder.results_path / data_root_path.name).with_suffix(".csv")
+        processed_lines = count_lines(results_path)
+        process_args.append(results_path.absolute())
+        process_args.append(processed_lines)
+        process_args_stringified = [str(arg) for arg in process_args]
+        print(f"Launching subprocess {process_args_stringified}")
+        stdout_logs_path = (encoder.results_path / data_root_path.name).with_suffix(".stdout")
+        stderr_logs_path = (encoder.results_path / data_root_path.name).with_suffix(".stderr")
+        print(f"Stdout logs will be saved in {stdout_logs_path.absolute()} and stderr logs will be saved to {stderr_logs_path.absolute()}")
+        with open(stdout_logs_path, "a+") as stdout_logs, open(stderr_logs_path, "a+") as stderr_logs:
+            exit_code = subprocess.call(process_args_stringified, stdout=stdout_logs, stderr=stderr_logs)
             print(f"{encoder.exe_path} subprocess exited with {exit_code}")
